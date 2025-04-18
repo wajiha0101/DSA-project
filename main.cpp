82cc1e687145f519d1f546dfb0e38c6cfd9aee15
Wajiha <149070831+wajiha0101@users.noreply.github.com>
2025-04-19 01:33:58 +0500

    sukudo board

--- main.cpp
+++ main.cpp
@@ -1,7 +1,48 @@
 #include<iostream>
+#include <iomanip>
+
 using namespace std;
+
+const int n = 9;
+int grid[n][n] = {
+    {5, 3, 0, 0, 7, 0, 0, 0, 0},
+    {6, 0, 0, 1, 9, 5, 0, 0, 0},
+    {0, 9, 8, 0, 0, 0, 0, 6, 0},
+    {8, 0, 0, 0, 6, 0, 0, 0, 3},
+    {4, 0, 0, 8, 0, 3, 0, 0, 1},
+    {7, 0, 0, 0, 2, 0, 0, 0, 6},
+    {0, 6, 0, 0, 0, 0, 2, 8, 0},
+    {0, 0, 0, 4, 1, 9, 0, 0, 5},
+    {0, 0, 0, 0, 8, 0, 0, 7, 9}
+};
+
+void printboard()
+{
+    for (int row = 0; row < n; row++)
+    {
+        if (row % 3 == 0 && row != 0)
+        {
+            cout<< "------+-------+------\n";
+        }
+
+        for (int col = 0; col < n; col++)
+        {
+            if (col % 3 == 0 && col != 0)
+            {
+                cout << "| ";
+            }
+
+            if (grid[row][col] == 0)
+                cout << setw(2) << ".";
+            else
+                cout << setw(2) << grid[row][col] ;
+        }
+        cout << endl;
+    }
+}
 
 int main()
 {
+    printboard();
 	return 0;
 }
