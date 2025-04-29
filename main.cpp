9e3b0c596c305097bdac844b55c8f21df89b7bdd
Wajiha <149070831+wajiha0101@users.noreply.github.com>
2025-04-29 15:39:17 +0500

    don't know

--- main.cpp
+++ main.cpp
@@ -17,34 +17,74 @@ void displayMenu() {
 
 
 void playSudoku(SudokuDB& db) {
-    string puzzleName;
-    cout << "Enter the name of the Sudoku puzzle to load: ";
-    cin >> puzzleName;
-
-    string puzzleData = db.loadPuzzle(puzzleName);
-    if (puzzleData.empty()) {
-       cout << "Puzzle not found in the database!" << endl;
+    vector<string> puzzles = db.getAllPuzzleNames();
+    if (puzzles.empty()) {
+        cout << "No puzzles in the database.\n";
         return;
     }
 
-    int board[9][9];
-    db.stringToBoard(puzzleData, board);
-
-    cout << "Loaded Sudoku Puzzle:" << endl;
-    printboard();
-
-    if (solvesudoku()) {
-        cout << "\nSolved Sudoku Board:" << endl;
-        printboard();
-
-        string solvedPuzzleData = db.boardToString(board);
-        db.savePuzzle(puzzleName + " Solved", solvedPuzzleData);
-        cout << "Solved puzzle saved to database." << endl;
+    cout << "Available puzzles:\n";
+    for (size_t i = 0; i < puzzles.size(); ++i) {
+        cout << i + 1 << ". " << puzzles[i] << "\n";
     }
-    else {
-       cout << "No solution exists for this Sudoku puzzle." << endl;
+
+    int choice;
+    cout << "Enter the number of the puzzle to load: ";
+    cin >> choice;
+
+    if (choice < 1 || choice > puzzles.size()) {
+        cout << "Invalid choice.\n";
+        return;
     }
+
+    string selectedPuzzle = puzzles[choice - 1];
+    string puzzleData = db.loadPuzzle(selectedPuzzle);
+
+    if (puzzleData.empty()) {
+        cout << "Puzzle not found in the database.\n";
+        return;
+    }
+
+    db.stringToBoard(puzzleData, grid);
+    cout << "\nPuzzle Loaded. Start solving:\n";
+
+    userSolve();  // Only this: user manually enters values in the grid
 }
+
+void seedSudokuPuzzles(SudokuDB& db) {
+    int easy[9][9] = {
+        {5,3,0, 0,7,0, 0,0,0},
+        {6,0,0, 1,9,5, 0,0,0},
+        {0,9,8, 0,0,0, 0,6,0},
+
+        {8,0,0, 0,6,0, 0,0,3},
+        {4,0,0, 8,0,3, 0,0,1},
+        {7,0,0, 0,2,0, 0,0,6},
+
+        {0,6,0, 0,0,0, 2,8,0},
+        {0,0,0, 4,1,9, 0,0,5},
+        {0,0,0, 0,8,0, 0,7,9}
+    };
+
+    int hard[9][9] = {
+        {0,0,0, 0,0,0, 0,1,2},
+        {0,0,0, 0,3,5, 0,0,0},
+        {0,0,0, 7,0,0, 0,0,0},
+
+        {0,0,0, 6,0,0, 0,0,3},
+        {0,0,0, 0,9,0, 0,0,0},
+        {8,0,0, 0,0,4, 0,0,0},
+
+        {0,0,0, 0,0,9, 0,0,0},
+        {0,0,0, 4,1,0, 0,0,0},
+        {2,7,0, 0,0,0, 0,0,0}
+    };
+
+    db.savePuzzle("easy1", db.boardToString(easy));
+    db.savePuzzle("hard1", db.boardToString(hard));
+    cout << "Seeded DB with easy1 and hard1 puzzles.\n";
+}
+
 
 
 void playTicTacToe() {
